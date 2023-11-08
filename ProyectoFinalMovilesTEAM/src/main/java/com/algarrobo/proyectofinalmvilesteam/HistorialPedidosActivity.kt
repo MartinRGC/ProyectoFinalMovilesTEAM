bedf40bcfd47d485b69e54e5f52d365a75d5386d
nayduramon <naydu.3005@gmail.com>
2023-11-07 19:02:00 -0500

    parte relaizada historial_pedidos

---
+++ ProyectoFinalMovilesTEAM/src/main/java/com/algarrobo/proyectofinalmvilesteam/HistorialPedidosActivity.kt
@@ -0,0 +1,12 @@
+package com.algarrobo.proyectofinalmvilesteam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistorialPedidosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historial_pedidos)
+
+    }
+}
--- ProyectoFinalMovilesTEAM/src/main/java/com/algarrobo/proyectofinalmvilesteam/UserActivity.kt
+++ ProyectoFinalMovilesTEAM/src/main/java/com/algarrobo/proyectofinalmvilesteam/UserActivity.kt
@@ -1,15 +1,23 @@
 package com.algarrobo.proyectofinalmvilesteam
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
+import android.widget.ImageView
 
 class UserActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+
+        val imagen: ImageView = findViewById(R.id.idHistorialPedido)
+
+        imagen.setOnClickListener{startActivity(Intent(this,HistorialPedidosActivity::class.java))
+        }
     }
     fun MenuUsuario(view: View){
         startActivity(Intent(this,PrincipalActivity::class.java))
