3f4c178446e06df7df5efcb72cb5305898e15b3a
Carlos-Celi <carlosfceli98@gmail.com>
2023-11-09 12:27:52 -0500

    Arreglos

--- ProyectoFinalMovilesTEAM/src/main/java/com/algarrobo/proyectofinalmvilesteam/UserActivity.kt
+++ ProyectoFinalMovilesTEAM/src/main/java/com/algarrobo/proyectofinalmvilesteam/UserActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
 import android.view.View
 import android.widget.ImageView
 
